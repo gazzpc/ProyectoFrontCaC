584a000c1dbed3dd65805bed8cb766d23389f3b8
CandeYudati <cande.yudati@gmail.com>
2024-05-21 17:17:21 -0300

    catalogoCande

---
+++ Pages/js/datos.js
@@ -0,0 +1,45 @@
+let data = [
+    {
+        "id": 1,
+        "name": "Margarita",
+        "desc": "vela en frasco de vidrio 100cc",
+        "precio": "$500",
+        "imagen": "https://"
+    },
+    {
+        "id": 2,
+        "name": "Hexágono",
+        "desc": "vela en cemento 50cc ",
+        "precio": "$500",
+    },
+    {
+        "id": 3,
+        "name": "Combo calido",
+        "desc": "dos velas corazón en cemento de 50cc + velita en vaso de 100cc",
+        "precio": "$500",
+    },
+    {
+        "id": 4,
+        "name": "Combo Living",
+        "desc": "dos velitas en rectangulo de concreto de 80cc ",
+        "precio": "$500",
+    },
+    {
+        "id": 5,
+        "name": "love",
+        "desc": "Velitas en corazón de concreto de 80cc ",
+        "precio": "$500",
+    },
+    {
+        "id": 6,
+        "name": "Corazón",
+        "desc": "Velita en corazón de concreto con glitter de 100cc ",
+        "precio": "$500",
+    },
+    {
+        "id": 7,
+        "name": "Vasito souvenir",
+        "desc": "velitas en vidrio de 20cc con inscripción",
+        "precio": "$500",
+    },
+]
---
+++ Pages/js/header.js
@@ -0,0 +1,19 @@
+
+
+let miCatalogo = ``
+
+for(let elemento of data){
+
+    miCatalogo = miCatalogo + `
+    
+    <a href="./Catalogo.html">
+                    <img src="./static/img/solido.jpg" alt="Shampoo sólido" class="productosimg">
+                    <div class="detalle-producto">
+                        <p class="titulo-producto">Shampoo sólido</p>
+                        <p class="subtitulo-producto">$500</p>
+                    </div>
+                </a>
+
+    `
+}
+
